89e3da1d7bc71153b04ec29542a4115161ca32a0
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-03-11 10:09:12 +0530

    update

---
+++ GUESS THE NUMBER/guess_the_number.py
@@ -0,0 +1,27 @@
+# Generate a number between 1 to 100
+
+# # loop
+# ask:to guess the number between 1 to 100
+# if guessed number is greater than actual number 
+#     print too high
+# elif guessed number is smaller than actual number
+#     print too low
+# elif guessed number == actual number
+#     print congratulations
+#     terminate
+# else print invalid choice
+
+import random
+
+gen_num=int(random.randint(1, 100))
+while True :
+    guess_num=int(input("Guess the number between 1 to 100: "))
+    if guess_num > gen_num:
+        print("Too high.")
+    elif guess_num < gen_num:
+        print("Too low.")
+    elif guess_num==gen_num:
+        print("Congratulations.")
+        break
+    else:
+        print("Invalid choice")
